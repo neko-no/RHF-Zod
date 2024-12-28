86c83e9f733cebf905085d530b901c36a8d77916
neko-no <101441574+neko-no@users.noreply.github.com>
2024-12-28 09:56:36 +0900

    #2 add: フォームの雛形を作成

--- src/components/MailForm/MailForm.tsx
+++ src/components/MailForm/MailForm.tsx
@@ -1,7 +1,44 @@
+"use client";
+
 import React from "react";
+import {
+  Form,
+  FormControl,
+  FormDescription,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "../ui/form";
+import { Input } from "../ui/input";
+import { Button } from "../ui/button";
 
 const MailForm = () => {
-  return <div>MailForm</div>;
+  return (
+    <div>
+      <Form>
+        <form onSubmit={() => {}} className="w-2/3 space-y-6">
+          <FormField
+            // control={form.control}
+            name="username"
+            render={({ field }) => (
+              <FormItem>
+                <FormLabel>Username</FormLabel>
+                <FormControl>
+                  <Input placeholder="shadcn" {...field} />
+                </FormControl>
+                <FormDescription>
+                  This is your public display name.
+                </FormDescription>
+                <FormMessage />
+              </FormItem>
+            )}
+          />
+          <Button type="submit">Submit</Button>
+        </form>
+      </Form>
+    </div>
+  );
 };
 
 export default MailForm;
