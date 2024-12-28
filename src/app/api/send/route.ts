36803a39e7a25d9288096971a17acfaa1805c043
neko-no <101441574+neko-no@users.noreply.github.com>
2024-12-28 16:23:50 +0900

    #3 add: メール本文に入力内容を記載する

--- src/app/api/send/route.ts
+++ src/app/api/send/route.ts
@@ -4,13 +4,14 @@ import { Resend } from "resend";
 
 const resend = new Resend(process.env.RESEND_API_KEY);
 
-export async function POST () {
+export async function POST (request: Request) {
+    const { username, subject, email, content} = await request.json();
     try {
         const {data, error} = await resend.emails.send({
             from: 'Acme <onboarding@resend.dev>',
             to: ['xxxx@gmail.com'],
-            subject: "開発相談",
-            react: EmailTemplate({username: "test_user", email: "test@gmail.com", content: "開発相談"}) as React.ReactElement
+            subject: subject,
+            react: EmailTemplate({username, email, content}) as React.ReactElement
         })
 
         if(error){
