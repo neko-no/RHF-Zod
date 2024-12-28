9cbdca13886fd0252cd84282aa41edaed6877c7d
neko-no <101441574+neko-no@users.noreply.github.com>
2024-12-28 10:54:45 +0900

    #2 zodを用いたバリデーションの適用

--- src/components/MailForm/MailForm.tsx
+++ src/components/MailForm/MailForm.tsx
@@ -13,9 +13,20 @@ import {
 import { Input } from "../ui/input";
 import { Button } from "../ui/button";
 import { useForm } from "react-hook-form";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { formSchema } from "@/lib/formSchema";
+import { Textarea } from "../ui/textarea";
 
 const MailForm = () => {
-  const form = useForm();
+  const form = useForm({
+    resolver: zodResolver(formSchema),
+    defaultValues: {
+      username: "",
+      subject: "",
+      email: "",
+      content: "",
+    },
+  });
 
   function onSubmit(values: any) {
     // Do something with the form values.
@@ -35,13 +46,49 @@ const MailForm = () => {
             name="username"
             render={({ field }) => (
               <FormItem>
-                <FormLabel>Username</FormLabel>
+                <FormLabel>ユーザー名</FormLabel>
                 <FormControl>
-                  <Input placeholder="shadcn" {...field} />
+                  <Input placeholder="ユーザー名" {...field} />
                 </FormControl>
-                <FormDescription>
-                  This is your public display name.
-                </FormDescription>
+                <FormMessage />
+              </FormItem>
+            )}
+          />
+          <FormField
+            control={form.control}
+            name="email"
+            render={({ field }) => (
+              <FormItem>
+                <FormLabel>メールアドレス</FormLabel>
+                <FormControl>
+                  <Input placeholder="メールアドレス" {...field} />
+                </FormControl>
+                <FormMessage />
+              </FormItem>
+            )}
+          />
+          <FormField
+            control={form.control}
+            name="subject"
+            render={({ field }) => (
+              <FormItem>
+                <FormLabel>主題</FormLabel>
+                <FormControl>
+                  <Input placeholder="主題" {...field} />
+                </FormControl>
+                <FormMessage />
+              </FormItem>
+            )}
+          />
+          <FormField
+            control={form.control}
+            name="content"
+            render={({ field }) => (
+              <FormItem>
+                <FormLabel>本文</FormLabel>
+                <FormControl>
+                  <Textarea placeholder="本文" {...field} />
+                </FormControl>
                 <FormMessage />
               </FormItem>
             )}
